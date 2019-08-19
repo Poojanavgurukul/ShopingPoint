6bfe876b3422fd246b2fd6ea9ee04cefd25fd2bf
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-19 09:33:31 +0530

    Add.product class and fields eg:-category,quantity,price

--- src/main/java/productTax/ProductDetail.java
+++ src/main/java/productTax/ProductDetail.java
@@ -1,4 +1,13 @@
 package productTax;
 
 public class ProductDetail {
+    public int quantity;
+    public double price;
+    public String category;
+
+    public ProductDetail(int quantity, double price, String category) {
+        this.quantity = quantity;
+        this.price = price;
+        this.category = category;
+    }
 }
