a913db6871ef96d015edbc08b5642e13ff13cf3c
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-19 10:13:03 +0530

    Add.two more fields in product detail class eg:-imported,name

--- src/main/java/productTax/ProductDetail.java
+++ src/main/java/productTax/ProductDetail.java
@@ -4,10 +4,14 @@ public class ProductDetail {
     public int quantity;
     public double price;
     public String category;
+    public boolean imported;
+    public String name;
 
-    public ProductDetail(int quantity, double price, String category) {
+    public ProductDetail(int quantity, double price, String category,boolean imported,String name) {
         this.quantity = quantity;
         this.price = price;
         this.category = category;
+        this.imported =imported;
+        this.name=name;
     }
 }
